c385977d7b6ff65371b179e1b374d40589e2f3f1
Colin Budka <colin.budka@gmail.com>
2018-06-26 23:19:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(String) {
-  if()
+  if(string.toLowerCase()==string){
+    return "I can't hear you!"
+  }
+  if(string.toUpperCase()==string){
+    return "YES INDEED!"
+  }
+  if(string=="I love you, Grandma."){
+    return "I love you, too."
+  }
+  else{
+    return null;
+  }
 }
